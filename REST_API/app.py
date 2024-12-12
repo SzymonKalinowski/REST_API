d4762645984c8efed5625afb015d090e76bc74ee
Szymon <u122_szykal_waw@technischools.com>
2024-12-12 21:18:16 +0100

    podpunkt2

--- REST_API/app.py
+++ REST_API/app.py
@@ -9,3 +9,11 @@ next_id = 1
 def get_users():
     return jsonify(list(users.values())), 200
 
+@app.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = users.get(user_id)
+    if user:
+        return jsonify(user), 200
+    else:
+        return '', 400
+
